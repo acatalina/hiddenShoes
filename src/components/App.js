c406cd8410b0016c015d13afd495364c83d3620d
acatalina <accarrasco@gmail.com>
2017-06-03 13:55:58 +0100

    add basic header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 
+import Header from './Header'; // eslint-disable-line no-unused-vars
+
 class App extends Component {
 	render() {
 		return (
 			<main>
-				<div>Hello world!</div>
+				<Header />
 				{this.props.children}
 			</main>
 		);
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+
+import './css/header.css';
+
+const Header = () => {
+	return (
+    <header>
+      <img alt="logo" />
+      <h1>Quality Trainers with the best prices, guaranteed!</h1>
+      <img alt="basket" />
+    </header>
+	);
+};
+
+export default Header;
