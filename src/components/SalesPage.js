71a72f850fdded6ccaf71a34a8aaca53f47ce427
acatalina <accarrasco@gmail.com>
2017-06-05 15:50:09 +0100

    abstraction of ProductsList

--- src/components/SalesPage.js
+++ src/components/SalesPage.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import { PropTypes } from 'prop-types';
 
-import ProductCard from './ProductCard'; // eslint-disable-line no-unused-vars
+import ProductsList from './ProductsList'; // eslint-disable-line no-unused-vars
 import Filter from './Filter'; // eslint-disable-line no-unused-vars
 import { filterByColour } from '../lib/helpers';
-
-import '../css/salespage.css';
 
 class SalesPage extends Component {
 	constructor(props) {
@@ -18,33 +16,18 @@ class SalesPage extends Component {
 		this.setFilter = this.setFilter.bind(this);
 	}
 	render() {
+		const {data} = this.props;
+		const {filterBy} = this.state; 
+
 		return (
 			<section>
 				<div className="container">
 					<h1>SALE TRAINERS</h1>
 					<Filter setFilter={this.setFilter} />
-					<ul className="product-list">
-						{this.generateProducts(this.props.data, this.state.filterBy)}
-					</ul>
+					<ProductsList data={filterByColour(data, filterBy)} />
 				</div>
 			</section>
 		);
-	}
-	generateProducts(products, colours) {
-		const filteredProducts = filterByColour(products, colours);
-
-		return filteredProducts.map((product, i) => {
-			return (
-				<ProductCard
-					key={i}
-					id={product.id}
-					name={product.name}
-					img={product.img}
-					were={product.were}
-					price={product.price}
-				/>
-			);
-		});
 	}
 	setFilter(newColour, method) {
 		if (method === 'add') {
